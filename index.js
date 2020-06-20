b8b9cb8461deaf524fad6321500866d7fcba5bad
EugeneShatalin <shatalin_ei@mail.ru>
2020-06-20 13:41:35 +0600

    Настроил и запустил сервер express.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const now = new Date().toLocaleString()
+const express = require('express') //переменная для подключения модюля express
 
-console.log(now)
+const app = express() //переменная отвечающая за все приложение, являющаяся результатом работы функции express
+
+
+//метод get первым параметром принимает какой-либо root для обработки
+//вторым параметром колбек функцию с двумя парамерами спомощью которовый даем ответ на get запрос
+app.get('/', (req, res) => {
+    res.end('Hello from node.js')
+})
+//метод listen определяющий на каком порту будет доступен наш сервер (1 параметр),
+// вторым параметром принимает функцию
+app.listen(3000, () => {
+    console.log('Server has started on port 3000...')
+})
